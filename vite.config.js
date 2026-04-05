e1b734519ab7537fdd9c1845d5aaee5e07a8ba0c
ThomasBui <thinhbg2812@gmail.com>
2026-04-06 04:35:21 +0700

    feat: add base in vite
    Signed-off-by: ThomasBui <thinhbg2812@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/maplestory-familiar-badge-tracker",
   plugins: [vue()],
-})
+});
